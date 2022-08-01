74cecdfb92b4734df31e28533a75b3b431edd049
Zaibonisha Mohamed <zsmohamed94@gmail.com>
2022-08-01 18:38:55 +0200

    add assert False

--- test_main.py
+++ test_main.py
@@ -29,6 +29,7 @@ def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
+    assert False
 
 
 def test_auth(client):
@@ -41,3 +42,5 @@ def test_auth(client):
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
+
+
